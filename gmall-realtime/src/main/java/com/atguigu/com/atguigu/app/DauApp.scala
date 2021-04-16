eacaba58a48b2530a55ae97675983fafd6018bb6
Taixiaopeng <69563441+Taixiaopeng@users.noreply.github.com>
2021-04-16 14:22:00 +0800

    2

--- gmall-realtime/src/main/java/com/atguigu/com/atguigu/app/DauApp.scala
+++ gmall-realtime/src/main/java/com/atguigu/com/atguigu/app/DauApp.scala
@@ -11,7 +11,9 @@ object DauApp {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("DauAPP").setMaster("local[*]")
     val ssc: StreamingContext = new StreamingContext(conf, Seconds(5))
-    val startupStream: InputDStream[ConsumerRecord[String, String]] = MyKafkaUtil.getKafkaStream(GmallConstants.KAFKA_TOPIC_STARTUP, ssc)
+    val startupStream: InputDStream[ConsumerRecord[String, String]] = MyKafkaUtil
+      .getKafkaStream(GmallConstants
+        .KAFKA_TOPIC_STARTUP, ssc)
     startupStream.foreachRDD(rdd => {
       rdd.foreach(record => println(record.value()))
     })
